35333c7081efe9c7458e9909a785f14c551e8dd9
tsionmehari <ttmeharid@gmail.com>
2024-07-27 12:08:53 -0400

    modified customers into customer

--- task_5.sql
+++ task_5.sql
@@ -1,3 +1,3 @@
--- Insert a single row into the customers table
-INSERT INTO Customers (customer_id, customer_name, email, address)
+INSERT INTO customer (customer_id, customer_name, email, address)
 VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
+
