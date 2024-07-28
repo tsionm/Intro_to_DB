ea8477f6fa1c602beb35ee6f486da8fb7e65175c
tsionmehari <ttmeharid@gmail.com>
2024-07-28 11:36:49 -0400

    modified ast4

--- task_4.sql
+++ task_4.sql
@@ -1,3 +1,11 @@
 USE alx_book_store;
-SHOW CREATE TABLE books;
 
+SELECT 
+  COLUMN_NAME,
+  COLUMN_TYPE,
+  DATA_TYPE
+FROM 
+  INFORMATION_SCHEMA.COLUMNS
+WHERE 
+  TABLE_SCHEMA = 'alx_book_store' 
+  AND TABLE_NAME = 'books';
